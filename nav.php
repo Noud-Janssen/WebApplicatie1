945d52d6a38a54692d5f505d497325b8489256c5
Noud-Janssen <117975100+Noud-Janssen@users.noreply.github.com>
2023-03-01 12:42:03 +0100

    working on nav

---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <h2 id="give"></h2>
+    <div class="link-wrapper">
+        <a href="index.php">Home</a>
+        <a href="menu.php">Menu</a>
+        <a href="contact.php">Contact</a>
+        <a href="login.php">login</a>
+    </div>
+</nav>
