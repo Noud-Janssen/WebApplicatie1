9be82f147dd1b582323cd2824a6cd35e7280c85d
Noud-Janssen <117975100+Noud-Janssen@users.noreply.github.com>
2023-03-06 11:42:20 +0100

    Made Menu Page

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title id="get">Dashboard</title>
     <link rel="stylesheet" href="css/main.css">
     
     <!--font-->
@@ -14,7 +14,7 @@
 </head>
 <body>
     <nav>
-        <h2>Home</h2>
+        <h2>Dashboard</h2>
         <div class="link-wrapper">
             <a href="index.php">Home</a>
             <a href="menu.php">Menu</a>
@@ -33,7 +33,6 @@
             <br>
             <input type="submit" value="Login">
         </form>
-        
     </div>
 </body>
 <script src="js/main.js"></script>
--- menu.php
+++ menu.php
@@ -19,9 +19,41 @@
             <a href="index.php">Home</a>
             <a href="menu.php">Menu</a>
             <a href="contact.php">Contact</a>
+            <a href="dashboard.php">Dashboard</a>
             <button onclick="toggleLogin()" class="login-button">Login</button>
         </div>
     </nav>
+    <div id="login">
+        <form action="">
+            <h4>Username</h4>
+            <input type="text" name="" id="">
+            <h4>Password</h4>
+            <input type="password" name="" id="">
+            <br>
+            <br>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+    <div class="menu-items">
+        <?php 
+            $is_vegan = true;
+            $vegan_div;
+            $name = "item naam";
+            $descr = "hbafjvvjvnkkaseli lsvbweBFlkkvlvlivvILw webhjasgdajssgdflhv;IVieglEWBFLJVB";
+            for ($i = 0; $i < 10; $i++) {
+                if ($is_vegan) {
+                    $vegan_div = "<div class='vegan-logo'>V</div>";
+                    $is_vegan = false;
+                } else {
+                    $vegan_div = "<div class='vegan-empty'></div>";
+                    $is_vegan = true;
+                }
+                $item = "<div class='menu-item'><div class=title><h2>".$name."</h2><h2>€".$i."</h2></div><h2 class='description'>".$descr."</h2>".$vegan_div."</div>";
+                echo $item;
+            }
+            
+        ?>
+    </div>
 </body>
 <script src="js/main.js"></script>
 </html>
